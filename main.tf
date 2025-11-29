7e1aacd1918997822726c03ff67aa53e133efb82
katehjd21 <katedrew2103@hotmail.co.uk>
2025-11-29 15:31:59 +0000

    updating terraform files

--- main.tf
+++ main.tf
@@ -42,8 +42,9 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # consider restricting this to your IP
+    cidr_blocks = ["0.0.0.0/0"]   
   }
+
 
   ingress {
     description = "HTTP"
